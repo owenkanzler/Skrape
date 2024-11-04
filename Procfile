9b216489657287f390592f8ce0f67f9bd4fa20d5
Owen Kanzler <oj.kanz1@gmail.com>
2024-11-03 20:43:10 -0600

    added gunicorn for production app

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 server/server.py
+web: gunicorn server.server:app
--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
 # server.py
 from flask import Flask, jsonify
 from routes.skrape import skrape_bp
+import os
 
 app = Flask(__name__)
 
 app.register_blueprint(skrape_bp, url_prefix='/skrape')
 
-if __name__ == '__main__':
-    app.run(port=8080)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # Default to 8080 if PORT not set
+    app.run(host='0.0.0.0', port=port)
